10bb46dcc7fb7f2ae6ed6f073dbf2fbd4501e550
杨超 <yangchao@babyfs.cn>
2018-04-24 20:21:14 +0800

    update revelations.py

--- revelations.py
+++ revelations.py
@@ -15,6 +15,45 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+timeSlots = [
+    "9:00-9:15",
+    "9:15-9:30",
+    "9:30-10:45",
+    "9:45-10:00",
+    "10:00-10:15",
+    "10:15-10:30",
+    "10:30-10:45",
+    "10:45-11:00",
+    "11:00-11:15",
+    "11:15-11:30",
+    "11:30-11:45",
+    "11:45-12:00",
+    "13:00-13:15",
+    "13:15-13:30",
+    "13:30-13:45",
+    "13:45-14:00",
+    "14:00-14:15",
+    "14:15-14:30",
+    "14:30-14:45",
+    "14:45-15:00",
+    "15:00-15:15",
+    "15:15-15:30",
+    "15:30-15:45",
+    "15:45-16:00",
+    "16:00-16:15",
+    "16:15-16:30",
+    "16:30-16:45",
+    "16:45-17:00",
+    "17:00-17:15",
+    "17:15-17:30",
+    "17:30-17:45",
+    "17:45-18:00",
+    "18:00-18:15",
+    "18:15-18:30",
+    "18:30-18:45",
+    "18:45-19:00"
+]
+
 class Schedule(db.Model):
 	__tablename__ = "schedule"
 
@@ -60,8 +99,14 @@ def arrange(date):
 
         # 获取外教当天所有时间段的可预约状态
         slots = db.session.query(Slot).filter(Slot.date==date, Slot.foreign_teacher==ftSchedule.foreign_teacher).all()
-        if slots:
-            arrangeList['slot_status'] = slots.slot_index
+        if slots and slots[0]:
+            print(slots[0])
+            arrangeList['slot_status'] = slots[0].slot_index
+        else:
+            initSlotStatus = []
+            for index in range(len(timeSlots)):
+                initSlotStatus.append(-2)
+            arrangeList['slot_status'] = initSlotStatus
         allArrange.append(arrangeList)
     db.session.close()
     return Response(json.dumps(allArrange), mimetype="text/json")
